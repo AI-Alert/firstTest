71db83975d69281256cf9864a545e85fae0a97db
AI-Alert <50011985+AI-Alert@users.noreply.github.com>
2022-06-16 11:13:17 +0200

    Adding download function

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const app = express()
 const PORT = config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware')
 
-
 app.use(fileUpload({}))
 app.use(corsMiddleware)
 app.use(express.json())
@@ -18,7 +17,7 @@ app.use("/api/files", fileRouter)
 
 const start = async () => {
     try {
-        await mongoose.connect(config.get("dbURL"), {
+        await mongoose.connect(config.get("dbUrl"), {
             useNewUrlParser:true,
             useUnifiedTopology:true
         })
